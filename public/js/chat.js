a51b95692f6ee018fbd891512e3244a9a356df93
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-03-07 20:15:17 +0200

    Sharing your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#message-form').addEventListener('submit' , (e) => {
 
     socket.emit('sendMessage', message);
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+        socket.emit('sendLocation', {
+            latitude,
+            longitude
+        });
+    });
+});
